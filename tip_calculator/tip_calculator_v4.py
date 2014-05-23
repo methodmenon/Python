5f68b2760289177d2ec4c608af0bcd674d4852e5
methodmenon <methodmenon@gmail.com>
2014-05-22 20:28:17 -0700

    commit 2
    v4 commit

--- tip_calculator/tip_calculator_v4.py
+++ tip_calculator/tip_calculator_v4.py
@@ -2,21 +2,25 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option("--m", "--meal_price", dest = "base_meal_price", type = "float")
-parser.add_option("--t", "--tip", dest = "tip_rate", type = "float")
-parser.add_option("--r", "--tax", dest = "tax_rate", type = "float")
+parser.add_option("-m", "--meal_price", dest = "base_meal_price", type = "float", help = "base cost of the meal")
+parser.add_option("-t", "--tip", dest = "tip_rate", type = "float")
+parser.add_option("-r", "--tax", dest = "tax_rate", type = "float", help = "percent tip to leave", default = .15)
 
 (options, args) = parser.parse_args()
+if not options.base_meal_price:
+	parser.error("Please enter the base price of the meal.")
+if not options.tax_rate:
+	parser.error("Please enter the tax rate")
 
 
 
 base_meal_price = options.base_meal_price
-tax_rate = options.tax_rate/100.0
-tip_rate = options.tip_rate/100.0
+tax_rate = options.tax_rate
+tip_rate = options.tip_rate
 
-tax_value = base_meal_price * tax_rate
+tax_value = base_meal_price * (tax_rate)
 meal_with_tax = base_meal_price + tax_value
-tip_value = tip_rate * base_meal_price
+tip_value = (tip_rate) * base_meal_price
 
 total = meal_with_tax + tip_value
 
